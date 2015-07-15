c0758c0271e618ee61e6db3007ba93341144e798
Yasin Uslu <nepjua@gmail.com>
2015-07-15 05:54:01 +0300

    keep nepjua.inc.zsh and ignore custom/ignored.sh

--- custom/example.zsh
+++
@@ -1,1 +0,0 @@
-alias cls="clear"
---
+++ nepjua.inc.zsh
@@ -0,0 +1,10 @@
+export PATH="$PATH:$HOME/code/bin"
+export PATH="$PATH:$HOME/prog/tools"
+
+source ~/prog/google-cloud-sdk/completion.zsh.inc
+source ~/prog/google-cloud-sdk/path.zsh.inc
+
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+
+alias cls="clear"
