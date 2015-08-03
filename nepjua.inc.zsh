11df0a4d7549dd51d47bd9062689008f000f6647
Ahmet Yasin Uslu <nepjua@nepjua.mynet>
2015-08-03 16:42:32 +0300

    update nepjua osx config

--- nepjua.inc.zsh
+++ nepjua.inc.zsh
@@ -2,10 +2,7 @@ export PATH="$PATH:$HOME/code/bin"
 export PATH="$PATH:$HOME/prog/tools"
 export PATH="$PATH:$HOME/.composer/vendor/bin"
 
-source ~/prog/google-cloud-sdk/completion.zsh.inc
-source ~/prog/google-cloud-sdk/path.zsh.inc
-
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+#source ~/prog/google-cloud-sdk/completion.zsh.inc
+#source ~/prog/google-cloud-sdk/path.zsh.inc
 
 alias cls="clear"
