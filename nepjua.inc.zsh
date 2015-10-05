823c52315421edecc9df655449532be1c0c6f96c
Yasin Uslu <nepjua@gmail.com>
2015-10-05 04:19:21 +0300

    android platform-tools

--- nepjua.inc.zsh
+++ nepjua.inc.zsh
@@ -1,6 +1,6 @@
 export PATH="$PATH:$HOME/code/bin"
-export PATH="$PATH:$HOME/prog/tools"
 export PATH="$PATH:$HOME/.composer/vendor/bin"
+export PATH="$PATH:$HOME/Library/Android/sdk/platform-tools"
 
 #source ~/prog/google-cloud-sdk/completion.zsh.inc
 #source ~/prog/google-cloud-sdk/path.zsh.inc
