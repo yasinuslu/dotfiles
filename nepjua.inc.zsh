c314d6660cbe2aa225a3b76c45e76641922b9502
Yasin Uslu <nepjua@gmail.com>
2015-10-23 01:16:18 +0300

    add android paths, add default push config for git

--- nepjua.inc.zsh
+++ nepjua.inc.zsh
@@ -2,6 +2,9 @@ export PATH="$PATH:$HOME/code/bin"
 export PATH="$PATH:$HOME/.composer/vendor/bin"
 export PATH="$PATH:$HOME/Library/Android/sdk/platform-tools"
 
+export ANDROID_NDK="$HOME/Library/Android/ndk/"
+export ANDROID_HOME="$HOME/Library/Android/sdk/"
+
 #source ~/prog/google-cloud-sdk/completion.zsh.inc
 #source ~/prog/google-cloud-sdk/path.zsh.inc
 
--- nepjua.install.sh
+++ nepjua.install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git config --global user.email "nepjua@gmail.com"
 git config --global user.name "Yasin Uslu"
+git config --global push.default simple
