efcd91162ab8e8a5dfa83a53cdcaa8c1ac3a6c9f
Yasin Uslu <nepjua@gmail.com>
2016-01-22 11:20:28 +0200

    dont know dont care

--- nepjua.inc.zsh
+++ nepjua.inc.zsh
@@ -5,6 +5,8 @@ export PATH="$PATH:$HOME/Library/Android/sdk/platform-tools"
 export ANDROID_NDK="$HOME/Library/Android/ndk/"
 export ANDROID_HOME="$HOME/Library/Android/sdk/"
 
+export JAVA_HOME="$(/usr/libexec/java_home -v 1.7)"
+
 #source ~/prog/google-cloud-sdk/completion.zsh.inc
 #source ~/prog/google-cloud-sdk/path.zsh.inc
 
