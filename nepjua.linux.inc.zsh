b882b3aa2a4e347fe2fb208463b7f6942919bd8d
Yasin Uslu <nepjua@gmail.com>
2016-03-28 19:09:13 +0300

    android sdk

--- nepjua.linux.inc.zsh
+++ nepjua.linux.inc.zsh
@@ -3,9 +3,12 @@ alias ync='yaourt --noconfirm'
 alias ynci='yaourt -S --noconfirm'
 
 export PATH="$PATH:$HOME/.meteor/"
-export PATH="$PATH:/opt/android-sdk/platform-tools/"
+export PATH="$PATH:$HOME/Android/Sdk/platform-tools/"
 export PATH="$PATH:$HOME/.composer/vendor/bin/";
 
 alias o='xdg-open';
 alias startvpn='systemctl start openvpn@nepjua-arch';
 alias stopvpn='systemctl stop openvpn@nepjua-arch';
+
+export ANDROID_HOME="$HOME/Android/Sdk";
+export REACT_EDITOR=atom;
