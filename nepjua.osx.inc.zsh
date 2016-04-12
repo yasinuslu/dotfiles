98f3d468301591320a12beb825654aa4c188e1d8
Yasin Uslu <nepjua@gmail.com>
2016-04-12 16:12:10 +0300

    create common inc

---
+++ nepjua.osx.inc.zsh
@@ -0,0 +1,12 @@
+export PATH="$PATH:$HOME/code/bin"
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+export PATH="$PATH:$HOME/Library/Android/sdk/platform-tools"
+
+export ANDROID_NDK="$HOME/Library/Android/ndk/"
+export ANDROID_HOME="$HOME/Library/Android/sdk/"
+export ANDROID_SDK="$HOME/Library/Android/sdk/"
+
+export JAVA_HOME="$(/usr/libexec/java_home -v 1.7)"
+
+#source ~/prog/google-cloud-sdk/completion.zsh.inc
+#source ~/prog/google-cloud-sdk/path.zsh.inc
