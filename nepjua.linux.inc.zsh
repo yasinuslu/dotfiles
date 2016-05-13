bc131d58b3e67b84f828316db74e88739a276b5c
Yasin Uslu <nepjua@gmail.com>
2016-05-13 15:31:48 +0300

    kmup

--- nepjua.linux.inc.zsh
+++ nepjua.linux.inc.zsh
@@ -9,6 +9,8 @@ export PATH="$PATH:$HOME/.config/composer/vendor/bin/";
 alias o='xdg-open';
 alias startvpn='systemctl start openvpn@nepjua-arch';
 alias stopvpn='systemctl stop openvpn@nepjua-arch';
+alias kmup='$HOME/code/github/meteor-up/index.js';
 
 export ANDROID_HOME="$HOME/Android/Sdk";
 export REACT_EDITOR=atom;
+export JAVA_HOME="/usr/lib/jvm/default";
