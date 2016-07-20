e272bde88c1709e35bf2ed26eea2841c56d390cb
Yasin Uslu <nepjua@gmail.com>
2016-07-20 13:18:38 +0300

    dont fetch any plugin for tmux-yank, tmux module got that covered now

--- install/tmux.sh
+++ install/tmux.sh
@@ -7,6 +7,3 @@ mv ~/.tmux ~/.tmux_old_nepjua 2>/dev/null
 mv ~/.tmux.conf ~/.tmux.conf_old_nepjua 2>/dev/null
 ln -s `pwd`/modules/tmux ~/.tmux 2>/dev/null
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf 2>/dev/null
-
-mkdir ~/.tmux-plugins/
-ln -s `pwd`/modules/tmux-yank ~/.tmux-plugins/tmux-yank 2>/dev/null
--- nepjua.inc.zsh
+++ nepjua.inc.zsh
@@ -1,5 +1,10 @@
 alias cls="clear"
 
+export PATH="$PATH:$HOME/.rvm/bin"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
 function gi() {
   curl -L -s "https://www.gitignore.io/api/$@"
 }
+
+export PATH=$PATH:~/bin
