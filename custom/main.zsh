e25f09b3f2d2ee1eea368bc37c28fc47d443ee25
Yasin Uslu <nepjua@gmail.com>
2017-02-10 01:32:59 +0200

    Add pacapt to ~/.bin
    - Add ~/.bin to hold binaries
    - Add pacapt, a nice package manager similar to pacman and able to use package manager we already have, apt in debian, brew in osx ...
    - Ensure we have `ag` installed using pacapt
    - Let fzf use ag and find hidden files

--- custom/main.zsh
+++ custom/main.zsh
@@ -2,8 +2,12 @@
 # if i'm gonna truncate a file with a redirection, it's gonna be my fault
 setopt clobber
 
+export PATH=${PATH}:${HOME}/.bin
+
 export CLICOLOR=1
 export LSCOLORS=ExGxBxDxCxEgEdxbxgxcxd
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_CTRL_T_COMMAND='ag --hidden --ignore .git -g ""'
 
 alias gst="git status"
 alias cls="clear"
--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+mkdir -p ${HOME}/.bin
+wget -O ${HOME}/.bin/pacapt \
+https://github.com/icy/pacapt/raw/ng/pacapt
+chmod 755 ${HOME}/.bin/pacapt
+
+${HOME}/.bin/pacapt -S ag
+
 # install zgen
-rm -rf ~/.zgen
+rm -rf ${HOME}/.zgen
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
-# install fzf
-rm -rf ~/.fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
+./scripts/install_fzf.sh
 ./tmux/install.sh
 ./vim/install.sh
-
 
 # update symlinks
 # this part is seperated because you might wanna update yout symlinks
