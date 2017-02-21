9eb8923db462aa142bc9f8d6b9507aac990090f0
Yasin Uslu <nepjua@gmail.com>
2017-02-21 23:39:59 +0300

    dont use ~/.bin/pacapt

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-curl -Lo ${HOME}/.local/bin/pacapt --create-dirs https://github.com/icy/pacapt/raw/ng/pacapt
-chmod 755 ${HOME}/.local/bin/pacapt
+export PACAPT=${HOME}/.local/bin/pacapt
+curl -Lo $PACAPT --create-dirs https://github.com/icy/pacapt/raw/ng/pacapt
+chmod 755 $PACAPT
 
-${HOME}/.bin/pacapt -S ag fish cmake
+$PACAPT -S ag fish cmake
 
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 ./scripts/install_fish.sh
