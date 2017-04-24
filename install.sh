53b35df22fc9aeda17bdba6d1af4e955f4ffdba9
Yasin Uslu <nepjua@gmail.com>
2017-04-25 00:35:28 +0300

    use rawgit url instead of git.io because of unknown ssl error

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ chmod 755 $PACAPT
 $PACAPT -S tmux vim ag fish cmake
 
 # install n
-curl -L https://git.io/n-install | bash
+N_URL=https://raw.githubusercontent.com/mklement0/n-install/stable/bin/n-install
+curl -L $N_URL | bash
 
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 ./scripts/install_fish.sh
 ./tmux/install.sh
 ./vim/install.sh
