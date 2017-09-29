6f7fe4ff95a7ab2ac37281a06a415a3532c5e560
Yasin Uslu <nepjua@gmail.com>
2017-09-29 23:14:06 +0300

    Add Android SDK path for linux

--- fish-plugin-nepjua/init.fish
+++ fish-plugin-nepjua/init.fish
@@ -13,10 +13,19 @@ source $NEPJUA_PATH/aliases/nepjua.fish
 
 switch (uname)
   case Linux
+    set ANDROID_HOME $HOME/Android/Sdk
+    set PATH $PATH $ANDROID_HOME/tools
+    set PATH $PATH $ANDROID_HOME/platform-tools
   case Darwin
     set ANDROID_HOME $HOME/Library/Android/sdk
     set PATH $PATH $ANDROID_HOME/tools
     set PATH $PATH $ANDROID_HOME/platform-tools
   case FreeBSD NetBSD DragonFly
   case '*'
+end
+
+if not type -q subl
+  if type -q subl3
+    alias subl="subl3"
+  end
 end
