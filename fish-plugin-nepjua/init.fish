5a651af6d5c81f18bbc12a0ece218e3fc123111a
Yasin Uslu <nepjua@gmail.com>
2017-10-29 22:18:50 +0300

    [WIP] Backup changes from arch-linux before reinstall

--- fish-plugin-nepjua/init.fish
+++ fish-plugin-nepjua/init.fish
@@ -6,26 +6,16 @@
 # * $dependencies  package dependencies
 # 
 
-set NEPJUA_PATH $HOME/.config/fisherman/nepjua
+set -x NEPJUA_PATH $HOME/.config/fisherman/nepjua
 
 source $NEPJUA_PATH/aliases/git.fish
 source $NEPJUA_PATH/aliases/nepjua.fish
 
 switch (uname)
   case Linux
-    set ANDROID_HOME $HOME/Android/Sdk
-    set PATH $PATH $ANDROID_HOME/tools
-    set PATH $PATH $ANDROID_HOME/platform-tools
+    source $NEPJUA_PATH/linux.init.fish
   case Darwin
-    set ANDROID_HOME $HOME/Library/Android/sdk
-    set PATH $PATH $ANDROID_HOME/tools
-    set PATH $PATH $ANDROID_HOME/platform-tools
+    source $NEPJUA_PATH/mac.init.fish
   case FreeBSD NetBSD DragonFly
   case '*'
-end
-
-if not type -q subl
-  if type -q subl3
-    alias subl="subl3"
-  end
 end
---
+++ fish-plugin-nepjua/mac.init.fish
@@ -0,0 +1,5 @@
+alias o="open"
+
+set ANDROID_HOME $HOME/Library/Android/sdk
+set PATH $PATH $ANDROID_HOME/tools
+set PATH $PATH $ANDROID_HOME/platform-tools
