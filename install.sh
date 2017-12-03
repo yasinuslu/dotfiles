457b5d1a5ae4ecc92abbc87a2ded78d06044520c
Yasin Uslu <nepjua@gmail.com>
2017-12-04 00:53:17 +0300

    Make PACAPT_COMMAND safer

--- install.sh
+++ install.sh
@@ -4,7 +4,14 @@ export PACAPT=${HOME}/.local/bin/pacapt
 curl -Lo $PACAPT --create-dirs https://github.com/icy/pacapt/raw/ng/pacapt
 chmod 755 $PACAPT
 
-${PACAPT_PREFIX-""} $PACAPT -S tmux vim the_silver_searcher fish cmake
+PACAPT_COMMAND=${PACAPT}
+
+if [[ ! -z "${PACAPT_PREFIX}" ]]
+then
+  PACAPT_COMMAND="${PACAPT_PREFIX:-""} $PACAPT"
+fi
+
+$PACAPT_COMMAND -S tmux vim the_silver_searcher fish cmake
 
 ./scripts/install_fish.sh
 ./tmux/install.sh
