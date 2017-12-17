7c4045db127a07f1c0e061432c4d0ef74c8fb99f
Yasin Uslu <nepjua@gmail.com>
2017-12-17 18:28:02 +0300

    Why not always use sudo

--- install.sh
+++ install.sh
@@ -4,12 +4,7 @@ export PACAPT=${HOME}/.local/bin/pacapt
 curl -Lo $PACAPT --create-dirs https://github.com/icy/pacapt/raw/ng/pacapt
 chmod 755 $PACAPT
 
-PACAPT_COMMAND=${PACAPT}
-
-if [[ ! -z "${PACAPT_PREFIX}" ]]
-then
-  PACAPT_COMMAND="${PACAPT_PREFIX:-""} $PACAPT"
-fi
+PACAPT_COMMAND="sudo ${PACAPT}"
 
 $PACAPT_COMMAND -S tmux vim fish cmake
 
