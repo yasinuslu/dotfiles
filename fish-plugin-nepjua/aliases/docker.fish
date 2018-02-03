00f8dc55f116609bb44c2746bd23b1c6e9cbd7bf
Yasin Uslu <nepjua@gmail.com>
2018-02-03 18:01:55 +0300

    Add dartisan for laravel

--- fish-plugin-nepjua/aliases/docker.fish
+++ fish-plugin-nepjua/aliases/docker.fish
@@ -1,1 +1,13 @@
 alias dcomposer='docker run --rm -it --tty -v $PWD:/app composer'
+alias dartisan='docker run --rm -it --tty -v $PWD:/app -w /app php:7-cli-alpine php artisan'
+function dartisan-serve
+    docker run \
+        --rm \
+        -it \
+        --tty \
+        -v $PWD:/app \
+        -w /app \
+        -p 8000:8000 \
+        php:7-cli-alpine \
+        php artisan serve --host=0.0.0.0 --port=8000
+end
