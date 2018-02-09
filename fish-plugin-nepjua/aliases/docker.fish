5994d70c1a2818ee89f09301fce68857f8ca8b1c
Alperen Elhan <alperen@elhan.org>
2018-02-10 00:38:30 +0300

    Add meteor-dev alias as dmeteor
    See: https://github.com/golden-garage/meteor-dev

--- fish-plugin-nepjua/aliases/docker.fish
+++ fish-plugin-nepjua/aliases/docker.fish
@@ -13,3 +13,34 @@ function dartisan-serve
 end
 alias doc='docker-compose'
 alias docp='docker-compose -f docker-compose.yml -f docker-compose-prod.yml'
+
+# dmeteor --dname=app --dport=5000 create app
+# dmeteor --dname=app --dport=5000 npm install
+# cd app
+# dmeteor --dname=app --dport=5000
+function dmeteor
+    set PORT 3000
+    set NAME "app"
+    set i 0
+    getopts $argv | while read -l key value
+        switch $key
+            case dport
+                set PORT $value
+                set i (math $i + 1)
+            case dname
+                set NAME $value
+                set i (math $i + 1)
+        end
+    end
+    if test $i -eq (count $argv)
+        set params
+    else
+        set params $argv[(math $i + 1)..-1]
+    end
+    docker run -it --rm --name=$NAME -v $PWD:/app -p $PORT:3000 golden/meteor-dev $params
+end
+
+# dmeteorx app <meteor command>
+function dmeteorx
+    docker exec -it $argv[1] meteor $argv[2..-1]
+end
