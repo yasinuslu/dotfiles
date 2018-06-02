669f6ab352a1924bcda214c76f89dcc86ea60c40
Yasin Uslu <nepjua@gmail.com>
2018-06-02 23:19:35 +0300

    Install go1.10.2

--- scripts/install_gvm.sh
+++ scripts/install_gvm.sh
@@ -4,8 +4,8 @@ function installGvm() {
   bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
   
   source $HOME/.gvm/scripts/gvm
-  gvm install go1.9 -B
-  gvm use go1.9
+  gvm install go1.10.2 -B
+  gvm use go1.10.2
 }
 
 installGvm
