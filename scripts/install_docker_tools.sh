e728d2f46066cf226d3291eba724cd4a329ccc31
Yasin Uslu <nepjua@gmail.com>
2018-07-08 16:35:48 +0300

    Install docker-compose

---
+++ scripts/install_docker_tools.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
