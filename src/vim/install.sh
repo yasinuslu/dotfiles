d15118ae4bd8915c315cf6a1e854d733bda03fad
Yasin Uslu <nepjua@gmail.com>
2018-08-29 17:38:37 +0300

    Use new cache path

--- src/vim/install.sh
+++ src/vim/install.sh
@@ -5,7 +5,7 @@ df_install_vim() {
   # remove current .vim-runtime
   rm -rf ${HOME}/.vim_runtime
   # clone vimrc into ${HOME}/.vim_runtime using our cache repo as reference
-  git clone --reference ${DF_SRC_PATH}/git-cache/vimrc git://github.com/amix/vimrc.git ${HOME}/.vim_runtime
+  git clone --reference ${DF_CACHE_PATH}/vimrc git://github.com/amix/vimrc.git ${HOME}/.vim_runtime
   sh ${HOME}/.vim_runtime/install_awesome_vimrc.sh
 
   ln -sf ${DF_SRC_PATH}/vim/main.vim ${HOME}/.vim_runtime/my_configs.vim
