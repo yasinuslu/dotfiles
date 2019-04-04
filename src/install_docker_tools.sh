bd8fdc25ed02a1e66a7e095e8d50af6420f61300
Yasin Uslu <nepjua@gmail.com>
2019-04-04 23:20:13 +0300

    fix: download docker-compose to local bin

--- src/install_docker_tools.sh
+++ src/install_docker_tools.sh
@@ -1,4 +1,8 @@
 df_install_docker_tools() {
-  sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose
-  sudo chmod +x /usr/bin/docker-compose
+  local binDir
+  
+  binDir="${HOME}/.local/bin"
+  
+  curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o "${binDir}/docker-compose"
+  chmod +x "${binDir}/docker-compose"
 }
