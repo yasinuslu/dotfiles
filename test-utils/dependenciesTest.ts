d9cfd84651ad90b589570d8be40e4d6c221824a9
Yasin Uslu <nepjua@gmail.com>
2019-05-11 22:13:28 +0200

    test: test availability of more tools

--- test-utils/dependenciesTest.ts
+++ test-utils/dependenciesTest.ts
@@ -1,6 +1,11 @@
 import execa from 'execa';
 
 export function dependenciesTest(execaOptions: execa.Options) {
+  it('asdf should be available', async () => {
+    const result = await execa(`asdf --version`, execaOptions);
+    expect(result.code).toEqual(0);
+  });
+
   it('node should be available', async () => {
     const result = await execa(`node --version`, execaOptions);
     expect(result.code).toEqual(0);
@@ -18,6 +23,31 @@ export function dependenciesTest(execaOptions: execa.Options) {
 
   it('go should be available', async () => {
     const result = await execa(`go version`, execaOptions);
+    expect(result.code).toEqual(0);
+  });
+
+  it('java should be available', async () => {
+    const result = await execa(`java -version`, execaOptions);
+    expect(result.code).toEqual(0);
+  });
+
+  it('fzf should be available', async () => {
+    const result = await execa(`fzf --version`, execaOptions);
+    expect(result.code).toEqual(0);
+  });
+
+  it('lsd should be available', async () => {
+    const result = await execa(`lsd --version`, execaOptions);
+    expect(result.code).toEqual(0);
+  });
+
+  it('docker-compose should be available', async () => {
+    const result = await execa(`docker-compose --version`, execaOptions);
+    expect(result.code).toEqual(0);
+  });
+
+  it('jq should be available', async () => {
+    const result = await execa(`jq --version`, execaOptions);
     expect(result.code).toEqual(0);
   });
 }
