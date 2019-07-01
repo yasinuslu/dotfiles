08e28284c0379dee31a10873800307b05264a045
Yasin Uslu <nepjua@gmail.com>
2019-07-01 15:52:13 +0200

    fix: don't just guess fish path

--- src/tmux/install.sh
+++ src/tmux/install.sh
@@ -8,7 +8,15 @@ df_install_tmux() {
   cp $HOME/.tmux/.tmux.conf.local $HOME/.tmux.conf.local
 
   ln -sf ${DF_SRC_PATH}/tmux/tmux.conf ${HOME}/.tmux.nepjua.conf
+
+  local genFilePath="${HOME}"/.tmux.nepjua.gen.conf
+  touch "${DF_PROJECT_PATH}/.gen/tmux.conf"
+  ln -sf "${DF_PROJECT_PATH}/.gen/tmux.conf" ${genFilePath}
+
+  echo "set-option -g default-shell $(which fish)" >> ${genFilePath}
+
   echo "source-file ${HOME}/.tmux.nepjua.conf" >> ${HOME}/.tmux.conf.local
+  echo "source-file ${genFilePath}" >> ${HOME}/.tmux.conf.local
 
   tmux source-file ${HOME}/.tmux.conf
 }
